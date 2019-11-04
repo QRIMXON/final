126e791456838ebc8ed688799fc5417087ec0c71
Nicholas Graff <nicholas_graff@insideranken.org>
2019-11-04 11:45:01 -0600

    11.4.19

--- labs/JS_CH04_LAB/change_maker/make_change.js
+++ labs/JS_CH04_LAB/change_maker/make_change.js
@@ -1,4 +1,30 @@
 var $ = function(id) {
     return document.getElementById(id);
 };
+var change = function getChange() {
+    var user_cents = parseInt($("cents").value);
 
+if(user_cents >= 25){
+var q = parseInt(user_cents / 25);
+$('quarters').value = q;
+user_cents = user_cents - (q * 25);
+}
+
+if(user_cents >= 10){
+    var d = parseInt(user_cents / 10);
+    $('dimes').value = d;
+    user_cents = user_cents - (d * 10);
+    }
+
+    if(user_cents >= 5){
+        var n = parseInt(user_cents / 5);
+        $('nickels').value = n;
+        user_cents = user_cents - (n * 5);
+        }
+
+        if(user_cents >= 1){
+            var p = parseInt(user_cents / 1);
+            $('pennies').value = p;
+            user_cents = user_cents - (p * 1);
+            }
+        }
--- labs/JS_CH04_LAB/sales_tax/sales_tax.js
+++ labs/JS_CH04_LAB/sales_tax/sales_tax.js
@@ -2,4 +2,10 @@ var $ = function (id) {
     return document.getElementById(id); 
 };
 
+function Calculate(){
+var subtotal = parseInt($("subtotal").value);    
+var tax_rate = parseInt($("tax_rate").value);
+$("sales_tax").value = subtotal * tax_rate / 100;
+$("total").value = subtotal + parseInt($("sales_tax").value);
+}
 
