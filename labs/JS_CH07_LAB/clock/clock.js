9fa5dec0cebe80cd2f9225bf80f8d28a1e9bc524
Nicholas Graff <nicholas_graff@insideranken.org>
2019-11-22 10:18:06 -0600

    12

--- labs/JS_CH07_LAB/clock/clock.js
+++ labs/JS_CH07_LAB/clock/clock.js
@@ -1,23 +1,27 @@
 "use strict";
 var $ = function(id) { return document.getElementById(id); };
 
-
+//global stop watch timer variable and elapsed time object
+var stopwatchTimer;
+var elapsedMinutes = 0;
+var elapsedSeconds = 0;
+var elapsedMilliseconds = 0;
 
 var displayCurrentTime = function() {
     var now = new Date();
     var hours = now.getHours();
-    var ampm = "AM"; 
+    var ampm = "AM"; // set default value
     
-    
-    if (hours > 12) { 
+    // correct hours and AM/PM value for display
+    if (hours > 12) { // convert from military time
         hours = hours - 12;
-        ampm = "pm";
-    } else { 
+        ampm = "PM";
+    } else { // adjust 12 noon and 12 midnight
          switch (hours) {
-            case 12: 
+            case 12: // noon
                 ampm = "PM";
                 break;
-            case 0: 
+            case 0:  // midnight
                 hours = 12;
                 ampm = "AM";
         }
