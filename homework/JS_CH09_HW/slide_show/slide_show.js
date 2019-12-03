d6e29a29890c7b810dfe5ee93d251b27af3a3714
Nicholas Graff <nicholas_graff@insideranken.org>
2019-12-03 08:10:52 -0600

    ch9hw

--- homework/JS_CH09_HW/slide_show/slide_show.js
+++ homework/JS_CH09_HW/slide_show/slide_show.js
@@ -1,28 +1,67 @@
 $(document).ready(function() {
 	// create an array of the slide images
-	var image, imageCache = [];
+	var image,imageCounter = 0, imageCache = [];
 	$("#slides img").each(function() {	
 		image = new Image();
         image.src = $(this).attr("src");
         image.title = $(this).attr("alt");
-        imageCache[imageCache.length] = image;
+		imageCache[imageCounter] = image;
+		imageCounter++;
    	});
 	
 	// Start slide show
 	var imageCounter = 0;
-	var nextImage, nextCaption;
-    var  timer = setInterval(
+	var nextImage;
+    setInterval(
         function () {
-        	$("#caption, #slide").fadeOut(1000,
+        	$("#caption").fadeOut(1000);
+			$("slide").fadeOut (1000,
 				function() {
-					imageCounter = (imageCounter + 1) % imageCache.length;
-		        	nextImage = imageCache[imageCounter];
-		        	nextCaption = nextImage.title;
-			    	$("#slide").attr("src", nextImage.src).fadeIn(1000);
-				    $("#caption").text(nextCaption).fadeIn(1000);		
-				}        	
-        	);
-        },
-    	3000);
-    
-});
+					imageCounter = (imageCounter + 1) %
+					imageCache.length;
+					nextImage = imageCache [imageCounter];
+					$("#slide").attr("src",
+					nextImage.src).fadeIn(1000);
+					$("caption").text(
+						nextImage.title).faceIn(1000);
+					}
+					);
+				},
+				3000);
+				});
+				$(document).ready(function() {
+					var nextSlide = $("#slides img:first-child");
+					var nextCaption;
+					var nextSlideSource;
+
+					// the function for running the slide show
+					var runSlideShow = function () {
+						$("#caption").fadeOut(1000);
+						$("#slide").fadeOut(1000,
+							function() {
+								if (nextSlide.next().length == 0) {
+									nextSlide = $("#slides img:first-child");
+								}
+								else {
+									nextSlide = nextSlide.next();
+								}
+								nextSlideSource = nextSlide.attr("src");
+								nextCaption = nextSlide.attr("alt");
+								$("#slide").attr("src",
+								nextSlideSource).fadeIn(1000);
+								$("#caption").text(nextCaption).fadeIn(1000);
+							}
+						);
+				};
+				var timer1 = setInterval (runSlideShow, 3000);
+
+				$("#slide").click(function() {
+					if (timer1 != null) {
+						clearInterval (timer1);
+						timer1 = null;
+					}
+					else {
+						timer1 = setInterval (runSlideShow, 3000);
+					}
+				});
+			});
